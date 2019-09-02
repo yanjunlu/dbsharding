4baaa537cee82ddc0506d5aebb114892fcaefb25
luyanjun <13802883173@139.com>
2019-09-02 11:18:05 +0800

    init sql

---
+++ src/main/resources/master.sql
@@ -0,0 +1,100 @@
+/*
+ Navicat Premium Data Transfer
+
+ Source Server         : localhost
+ Source Server Type    : MySQL
+ Source Server Version : 50727
+ Source Host           : localhost:3306
+ Source Schema         : master
+
+ Target Server Type    : MySQL
+ Target Server Version : 50727
+ File Encoding         : 65001
+
+ Date: 02/09/2019 11:16:16
+*/
+
+SET NAMES utf8mb4;
+SET FOREIGN_KEY_CHECKS = 0;
+
+-- ----------------------------
+-- Table structure for t_order_0
+-- ----------------------------
+DROP TABLE IF EXISTS `t_order_0`;
+CREATE TABLE `t_order_0`  (
+  `order_id` bigint(20) NOT NULL,
+  `remark` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`order_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of t_order_0
+-- ----------------------------
+INSERT INTO `t_order_0` VALUES (2, 'master');
+
+-- ----------------------------
+-- Table structure for t_order_1
+-- ----------------------------
+DROP TABLE IF EXISTS `t_order_1`;
+CREATE TABLE `t_order_1`  (
+  `order_id` bigint(20) NOT NULL,
+  `remark` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`order_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of t_order_1
+-- ----------------------------
+INSERT INTO `t_order_1` VALUES (1, 'master');
+
+-- ----------------------------
+-- Table structure for t_order_item_0
+-- ----------------------------
+DROP TABLE IF EXISTS `t_order_item_0`;
+CREATE TABLE `t_order_item_0`  (
+  `order_item_id` bigint(20) NOT NULL,
+  `order_id` bigint(20) NOT NULL,
+  `remark` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`order_item_id`) USING BTREE,
+  INDEX `order_id`(`order_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of t_order_item_0
+-- ----------------------------
+INSERT INTO `t_order_item_0` VALUES (22, 2, 'master');
+
+-- ----------------------------
+-- Table structure for t_order_item_1
+-- ----------------------------
+DROP TABLE IF EXISTS `t_order_item_1`;
+CREATE TABLE `t_order_item_1`  (
+  `order_item_id` bigint(20) NOT NULL,
+  `order_id` bigint(20) NOT NULL,
+  `remark` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  PRIMARY KEY (`order_item_id`) USING BTREE,
+  INDEX `order_id`(`order_id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of t_order_item_1
+-- ----------------------------
+INSERT INTO `t_order_item_1` VALUES (11, 1, 'master');
+
+-- ----------------------------
+-- Table structure for test
+-- ----------------------------
+DROP TABLE IF EXISTS `test`;
+CREATE TABLE `test`  (
+  `id` bigint(20) NOT NULL,
+  PRIMARY KEY (`id`) USING BTREE
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of test
+-- ----------------------------
+INSERT INTO `test` VALUES (1);
+INSERT INTO `test` VALUES (2);
+INSERT INTO `test` VALUES (3);
+
+SET FOREIGN_KEY_CHECKS = 1;
