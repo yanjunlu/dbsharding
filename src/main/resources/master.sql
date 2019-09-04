fb490fa53192629e6e0e4060970e77aed28b5b37
luyanjun <13802883173@139.com>
2019-09-04 14:31:43 +0800

    utf8mb4

--- src/main/resources/master.sql
+++ src/main/resources/master.sql
@@ -23,9 +23,9 @@ SET FOREIGN_KEY_CHECKS = 0;
 DROP TABLE IF EXISTS `t_order_0`;
 CREATE TABLE `t_order_0`  (
   `order_id` bigint(20) NOT NULL,
-  `remark` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `remark` varchar(100) CHARACTER SET utf8 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
   PRIMARY KEY (`order_id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of t_order_0
@@ -38,9 +38,9 @@ INSERT INTO `t_order_0` VALUES (2, 'master');
 DROP TABLE IF EXISTS `t_order_1`;
 CREATE TABLE `t_order_1`  (
   `order_id` bigint(20) NOT NULL,
-  `remark` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `remark` varchar(100) CHARACTER SET utf8 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
   PRIMARY KEY (`order_id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of t_order_1
@@ -54,10 +54,10 @@ DROP TABLE IF EXISTS `t_order_item_0`;
 CREATE TABLE `t_order_item_0`  (
   `order_item_id` bigint(20) NOT NULL,
   `order_id` bigint(20) NOT NULL,
-  `remark` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `remark` varchar(100) CHARACTER SET utf8 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
   PRIMARY KEY (`order_item_id`) USING BTREE,
   INDEX `order_id`(`order_id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of t_order_item_0
@@ -71,10 +71,10 @@ DROP TABLE IF EXISTS `t_order_item_1`;
 CREATE TABLE `t_order_item_1`  (
   `order_item_id` bigint(20) NOT NULL,
   `order_id` bigint(20) NOT NULL,
-  `remark` varchar(100) CHARACTER SET utf8 COLLATE utf8_general_ci NULL DEFAULT NULL,
+  `remark` varchar(100) CHARACTER SET utf8 COLLATE utf8mb4_general_ci NULL DEFAULT NULL,
   PRIMARY KEY (`order_item_id`) USING BTREE,
   INDEX `order_id`(`order_id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of t_order_item_1
@@ -88,7 +88,7 @@ DROP TABLE IF EXISTS `test`;
 CREATE TABLE `test`  (
   `id` bigint(20) NOT NULL,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8_general_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB CHARACTER SET = utf8 COLLATE = utf8mb4_general_ci ROW_FORMAT = Dynamic;
 
 -- ----------------------------
 -- Records of test
